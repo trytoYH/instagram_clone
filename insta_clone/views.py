0db3329a8a790a9a862b0ea5d2a7e87b80c62817
trytoYH <trytolight@gmail.com>
2023-07-18 05:21:08 +0900

    init

---
+++ insta_clone/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from content.models import Feed
+
+
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all()
+        return render(request, 'insta_clone/main.html', context=dict(feed_list=feed_list))
+    

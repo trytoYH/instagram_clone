1dc38693cab0bb0b06fa93eecc8f65f523003048
trytoYH <trytolight@gmail.com>
2023-08-02 21:22:06 +0900

    upload func

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -125,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+## Media files
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- insta_clone/urls.py
+++ insta_clone/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import Main
+from .views import Main, UploadFeed
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Main.as_view()),
+    path('content/upload', UploadFeed.as_view())
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
